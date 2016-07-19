ccf983779f51f8710ff7cef473c2c0c34a58ae06
Michał <mduzin@o2.pl>
2016-07-19 20:05:02 +0200

    Prepare source files for data logging

---
+++ src/file_log.c
@@ -0,0 +1,18 @@
+/*
+ * file_log.c
+ *
+ *  Created on: 19 lip 2016
+ *      Author: M
+ */
+
+#include "file_log.h"
+
+typedef struct _FILE_LOG
+{
+	char* filename;
+	FILE *file;
+
+	//pointery ADT do danych regulatora i obiektu
+}FILE_LOG;
+
+
---
+++ src/file_log.h
@@ -0,0 +1,17 @@
+/*
+ * file_log.h
+ *
+ *  Created on: 19 lip 2016
+ *      Author: M
+ */
+
+#ifndef FILE_LOG_H_
+#define FILE_LOG_H_
+
+//ADT
+typedef struct _FILE_LOG* FILE_LOG_PTR;
+
+//API for ADT
+
+
+#endif /* FILE_LOG_H_ */
